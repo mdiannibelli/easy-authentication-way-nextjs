2be62c984223255d75ed5b110a543a06c7aaf8fd
Marcos <mdiannibelli@gmail.com>
2025-03-18 19:41:49 -0300

    add(-):env template

--- prisma/migrations/20250318224044_user_table/migration.sql
+++ prisma/migrations/20250318224044_user_table/migration.sql
@@ -1,13 +1,17 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("userId")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_userId_key" ON "User"("userId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
